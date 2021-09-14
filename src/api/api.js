1ef450b8ea9ef04bbb73284622aff05f59d73967
HericMendez <heric.mendez00@gmail.com>
2021-09-14 19:36:33 -0300

    URL base alterada

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const api = axios.create({baseURL: 'http://localhost:1337'})
+export const api = axios.create({baseURL: 'https://pokedex-nodejs-backend.herokuapp.com'})
 
 export const busca = async(url, setDado) =>{
     const resposta = await api.get(url)
